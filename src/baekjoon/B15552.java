51603ae149afb5530ad8f4597894ac0deb11bf3d
Kim Minjuicy <Kim Minjuicy@Kimminjoo>
2018-08-24 00:52:37 +0900

    15552 commit

---
+++ src/baekjoon/B15552.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B15552 {
+
+	public static void main(String[] args)  throws IOException{
+	
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<T; i++) {
+			
+			String[] str = br.readLine().split(" "); 
+			bw.write(Integer.parseInt(str[0]) + Integer.parseInt(str[1]) +"\n");
+			
+		}
+		
+		bw.flush();
+		bw.close();
+		
+	
+	}
+	
+}

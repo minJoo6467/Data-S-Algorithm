adea6d3cfbb559f666a90a2b589ee8c6fef64b0b
Kim Minjuicy <Kim Minjuicy@Kimminjoo>
2018-08-26 23:51:20 +0900

    Sum

---
+++ src/algorithm/Sum.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	static int sum(int x) {
+		if(x <=1) {
+			return 1;
+		}else {
+		
+			return sum(x-1) + x;
+		}
+		
+	}
+	public static void main(String[] args) {
+		
+		System.out.println("1부터 x까지의 합을 구합니다.");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int x = scan.nextInt();
+		
+		System.out.println("1부터 " + x +"까지의 합은 "+ sum(x));
+	}
+
+}

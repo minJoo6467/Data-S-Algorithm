430fa31a75efb29d9d382e348daadbf0893cc01e
Kim Minjuicy <Kim Minjuicy@Kimminjoo>
2018-08-27 14:39:31 +0900

    sort

---
+++ src/baekjoon/B10817.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B10817 {
+
+	//Arrays.sort함수 이용
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[3];
+		
+		for(int i=0; i< arr.length; i++) {
+			
+			arr[i] = sc.nextInt();
+		
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
+		
+	}
+
+}

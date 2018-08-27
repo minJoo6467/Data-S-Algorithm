267c6d1179d1526562a904f1c7812290284b8ef6
Kim Minjuicy <Kim Minjuicy@Kimminjoo>
2018-08-27 17:07:02 +0900

    bubble modify

--- src/baekjoon/B2839.java
+++ src/baekjoon/B2839.java
@@ -4,35 +4,38 @@ import java.util.Scanner;
 
 public class B2839 {
 
-	public static void main(String[] args) {
+	static void swap(int[]a, int idx1, int idx2) {
+		int temp = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = temp;
 		
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		int three = 0; 
-		int five=0;
-		int result = 0;
-		
-		if(N >= 3 && N<=5000) {
-			
-			while(N % 5 !=0 && N >0) {
-				
-				N -= 3;
-				three ++;
-			}
-			
-			if(N <0) {
-				System.out.println(-1);
-			}else {
-				
-				five = N /5; //5로 나누었을때 몫이 들어감!
-				
-				result = three + five;
-				
-				System.out.println("봉지의 수는? " + result);
-			}
-			
-		}
-
 	}
 
+	static void bubbleSort(int[]a, int N) {
+		
+		for(int i=0; i< N-1; i++) {
+			for(int j = 0; j < N-1; j++) {
+				
+				if(a[j] > a[j+1]) {
+					swap(a, j , j+1);
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); //수의 개수
+		int[] arr = new int[N];
+		
+		for(int i=0; i< N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		bubbleSort(arr, N);
+		
+		for(int i=0; i<N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 }

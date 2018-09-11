5cddd0a05036629cea016da98d86fef2f621f4a2
Kim Minjuicy <Kim Minjuicy@Kimminjoo>
2018-09-11 14:55:10 +0900

    mode print

---
+++ src/baekjoon/B1152.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B1152 {
+
+	//À½°è
+	public static void main(String[] args) {
+		
+		Scanner scan  = new Scanner(System.in);
+		
+		String asc = "1 2 3 4 5 6 7 8";
+		String desc = "8 7 6 5 4 3 2 1";
+		
+		String in = scan.nextLine();
+				
+		if(in.equals(asc)) {
+			
+			System.out.println("ascending");
+		
+		}else if(in.equals(desc)) {
+			
+			System.out.println("descending");
+		
+		}else {
+			System.out.println("mixed");
+		}
+	}
+}
